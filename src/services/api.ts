91331cd76ff41a3f19d5780dd332753858494845
Asif11222 <shariarasif420@gmail.com>
2026-05-18 05:47:41 +0600

    Initial commit for frontend connected backend with frontend using axios and created regestration

---
+++ src/services/api.ts
@@ -0,0 +1,12 @@
+// src/services/api.ts
+
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL: 'http://localhost:3000',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default API;
